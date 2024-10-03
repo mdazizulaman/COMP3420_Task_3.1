bd92171ef8b6f4a52d9a8c56ab26b1058ea5a75d
mdazizulaman <98475994+mdazizulaman@users.noreply.github.com>
2024-10-04 01:19:50 +1000

    Completed Task 1

--- a3_1.py
+++ a3_1.py
@@ -6,15 +6,42 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 import collections
-import pandas as pd
+import pandas as pd 
 
 # Task 1 (5 marks)
 def topN_pos(csv_file_path, N):
-    """
-    Example:
-    >>> topN_pos('train.csv', 3)
-    output would look like [(noun1, 22), (noun2, 10), ...]
-    """
+    # Reading the CSV file
+  dataset = pd.read_csv(csv_file_path)
+
+  # Extracting questions
+  questions = dataset['qtext'].tolist()
+  print(questions)
+
+  # Combining questions into a single string
+  unique_questions = set(questions)
+  all_text = " ".join(unique_questions)
+
+  # Tokenizing sentences
+  sentences = sent_tokenize(all_text)
+  # Create a list of lists for words in each sentence
+  tokenized_sentences = [word_tokenize(sentence) for sentence in sentences]
+  #Cleaning the tokens
+  words = [word.lower() for sentence in tokenized_sentences for word in sentence if word.isalpha()]
+  print(words)
+  # Using NLTK's pos_tag_sents with Universal tagset for efficient tagging
+  pos_tags = nltk.pos_tag_sents(tokenized_sentences, tagset='universal')
+
+  # Counting occurrences of each noun
+  noun_counts = collections.Counter()
+  for tags in pos_tags:
+     for word, tag  in tags:
+        if tag =='NOUN':
+           noun_counts[word] += 1
+
+  # Getting the top N most common nouns
+  top_n_nouns = noun_counts.most_common(N)
+  #returning the result
+  return top_n_nouns
 
 
 # Task 2 (5 marks)
